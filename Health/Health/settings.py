169649e004c9fe7b4ec31c586a49d4c97bed5db0
Afkborn <kalaybilgehan60@gmail.com>
2022-07-20 19:23:33 +0300

    secret

--- Health/Health/settings.py
+++ Health/Health/settings.py
@@ -23,7 +23,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from ...SECRETS import *
+from .SECRETS import *
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
